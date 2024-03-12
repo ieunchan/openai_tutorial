a8ed651a9d0ed4846bad0b4bb465fe76e4795fa2
eunchan lee <jsmith26@gmail.com>
2024-03-12 14:24:03 +0900

    image gen

---
+++ openai_tutorial_image.py
@@ -0,0 +1,28 @@
+from PIL import Image
+from dotenv import load_dotenv
+import os
+from openai import OpenAI
+import requests
+
+
+load_dotenv()
+openai_api_key=os.getenv("OPENAI_API_KEY")
+MODEL="dall-e-3"
+
+client = OpenAI(api_key=openai_api_key)
+
+response = client.images.generate(
+    model=MODEL,
+    prompt="",
+    size="1024x1024",
+    quality="standard",
+    n=1,
+)
+image_url = response.data[0].url
+
+# 저장 파일 이름 설정
+filename = "image.jpg"
+response = requests.get(image_url)
+with open(filename,'wb') as f:
+    f.write(response.content)
+Image.open(filename)
